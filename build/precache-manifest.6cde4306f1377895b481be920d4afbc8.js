45023fdcc4b0c6a3b8c1b45848d10d63a67b9d22
Leonardo Ramos <leo.ramos.ari@gmail.com>
2020-08-10 10:09:23 -0500

    change

---
+++ build/precache-manifest.6cde4306f1377895b481be920d4afbc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4073ede2e92dc6819a8e406ee2bf26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f964b9963f0343d22bf0",
+    "url": "/static/css/2.a0d554f4.chunk.css"
+  },
+  {
+    "revision": "877209acf7f7773b7c28",
+    "url": "/static/css/main.a3eedea3.chunk.css"
+  },
+  {
+    "revision": "f964b9963f0343d22bf0",
+    "url": "/static/js/2.ab3ababd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ab3ababd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877209acf7f7773b7c28",
+    "url": "/static/js/main.0059da86.chunk.js"
+  },
+  {
+    "revision": "7311f5b841b219704094",
+    "url": "/static/js/runtime-main.acfe9a0b.js"
+  }
+]);
